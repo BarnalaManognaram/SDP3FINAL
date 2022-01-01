dd25c7992d679e14771ca1bde5f3835377b61362
Manognaram <190030140@kluniversity.in>
2022-01-01 10:08:41 +0530

    Login/singup

--- homestay/src/main/java/com/example/demo/HomeStayController.java
+++ homestay/src/main/java/com/example/demo/HomeStayController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
@@ -23,13 +24,43 @@ public class HomeStayController {
 	
 	 @Autowired HttpSession session;
 	
-	 
+
+		@Autowired UserService uservice;
 	
 	
 	@RequestMapping("/")  
 	public String login(Model model)  
 	{
 		return "login.html";
+	}
+	@RequestMapping("/signup")  
+	public String signup(@RequestParam("uname") String name,@RequestParam("email") String email,@RequestParam("pass") String pwd)  
+	{
+		
+		User u=new User();
+		u.setUname(name);
+		u.setEmail(email);
+		u.setPass(pwd);
+		uservice.savecustomer(u);
+		return "redirect:/";
+	}
+	@RequestMapping("/signin")
+	public String Signin(@RequestParam("uname") String email,@RequestParam("pass") String pwd) {
+		try {
+		User u=uservice.getCustomer(email);
+		String pwd2=u.getPass();
+		if(pwd2.matches(pwd)) {
+			return "redirect:/home";
+		}
+		else {
+			return "redirect:/";
+		}
+		}
+		catch(Exception e) {
+			return "redirect:/";
+		}
+		
+		
 	}
 	
 	@RequestMapping("/home")  
@@ -62,6 +93,7 @@ public class HomeStayController {
 	{
 		return "thankyou.html";
 	}
+
 	
 	
 }
